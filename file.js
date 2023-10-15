25033f6de2d55f6a257cf200f1b21a77ae376ef6
Mukunzijames <mukunzindahiro@gmail.com>
2023-10-15 11:49:35 -0700

    changes

--- file.js
+++ file.js
@@ -1,3 +1,13 @@
 for (const iterator of object) {
     
 }
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
+ for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+ }
