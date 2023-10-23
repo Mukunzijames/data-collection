07490eef7152c53b19336c41a177d36ffc7816d0
Mukunzijames <mukunzindahiro@gmail.com>
2023-10-23 03:30:39 -0700

    changes

--- console.log.js
+++ console.log.js
@@ -1,2 +1,8 @@
 console.log(1,2,3)
 alert ('welcome to our homepage')
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
