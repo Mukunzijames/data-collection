c134f5d3ed479c48fd34d8c354da5aadd509bc3e
Mukunzijames <mukunzindahiro@gmail.com>
2023-10-26 04:35:57 -0700

    changes

---
+++ awesomemessage.js
@@ -0,0 +1,10 @@
+// Declare and assign values to variables
+var firstName = "Julia";
+var interest = "cats";
+var hobby = "play video games";
+
+// Create an awesome message using string concatenation
+var awesomeMessage = "Hi, my name is " + firstName + ". I love " + interest + ". In my spare time, I like to " + hobby + ".";
+
+// Print the awesome message to the console
+console.log(awesomeMessage);
