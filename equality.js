cdbc29e0b4b611cb189d20b8868e2a5977eb46d6
Mukunzijames <mukunzindahiro@gmail.com>
2023-10-26 04:12:47 -0700

    changes

---
+++ equality.js
@@ -0,0 +1,10 @@
+//  So far, you’ve seen how you can use == and != 
+// to compare numbers and strings for equality. However, if you use == and != 
+// in situations where the values that you're comparing have different data-types,
+//  it can lead to some interesting results. For example,
+
+// type coreios
+
+1!==false
+
+true!==1
