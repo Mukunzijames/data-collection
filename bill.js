be534a827e0ca07624526f2beaa8c2798c3235a9
Mukunzijames <mukunzindahiro@gmail.com>
2023-10-26 04:28:53 -0700

    changes

---
+++ bill.js
@@ -0,0 +1,11 @@
+// Calculate the bill
+var bill = 10.25 + 3.99 + 7.15;
+
+// Calculate the tip (15% in decimal form is 0.15)
+var tip = bill * 0.15;
+
+// Calculate the total by adding the bill and tip
+var total = bill + tip;
+
+// Print the total to the JavaScript console
+console.log(total);
