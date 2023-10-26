b2093f88625a1330db4b0c36df89ac4261bec686
Mukunzijames <mukunzindahiro@gmail.com>
2023-10-26 03:58:10 -0700

    changes

--- console.log.js
+++ console.log.js
@@ -1,5 +1,7 @@
 console.log(1,2,3)
 alert ('welcome to our homepage')
+
+
 for (const key in object) {
     if (Object.hasOwnProperty.call(object, key)) {
         const element = object[key];
@@ -13,7 +15,9 @@ for (let index = 0; index < array.length; index++) {
 array.forEach(element => {
     
 });
+
 for(let first of second) {third}
+
 for (const key in object) {
     if (Object.hasOwnProperty.call(object, key)) {
         const element = object[key];
