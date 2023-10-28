411bfc5c974a4100f7200293e3107226f183c3e0
Mukunzijames <mukunzindahiro@gmail.com>
2023-10-28 09:36:27 -0700

    changes

---
+++ mystery.js
@@ -0,0 +1,25 @@
+let room = "ballroom"; // Change this to test different scenarios
+let suspect = "Mr. Kalehoff";
+
+let weapon;
+
+if (room === "ballroom") {
+  weapon = "poison";
+} else if (room === "gallery") {
+  weapon = "trophy";
+} else if (room === "billiards room") {
+  weapon = "pool stick";
+} else if (room === "dining room") {
+  weapon = "knife";
+}
+
+let solved = (room === "ballroom" && suspect === "Mr. Kalehoff") ||
+             (room === "gallery" && suspect === "Ms. Van Cleve") ||
+             (room === "billiards room" && suspect === "Mrs. Sparr") ||
+             (room === "dining room" && suspect === "Mr. Parkes");
+
+if (solved) {
+  console.log(`${suspect} did it in the ${room} with the ${weapon}!`);
+} else {
+  console.log("The mystery remains unsolved.");
+}
